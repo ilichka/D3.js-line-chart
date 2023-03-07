1fe82010da54346a09db10dc4c93389976e3b1eb
Ilya Parkhomenko <apapa2033@mail.ru>
2023-03-07 15:09:30 +0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.3f8849c2a817ff36e27cb095c1a8a574.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375e0ed1073658d8e763cb3edc96a5c5",
+    "url": "/D3.js-line-chart/index.html"
+  },
+  {
+    "revision": "84fbc40af1662412f809",
+    "url": "/D3.js-line-chart/static/css/main.136b07a2.chunk.css"
+  },
+  {
+    "revision": "994e7c7e8d1a4e9a41e0",
+    "url": "/D3.js-line-chart/static/js/2.26566a4a.chunk.js"
+  },
+  {
+    "revision": "84fbc40af1662412f809",
+    "url": "/D3.js-line-chart/static/js/main.f8091769.chunk.js"
+  },
+  {
+    "revision": "aa361ece0167bdba3bc4",
+    "url": "/D3.js-line-chart/static/js/runtime~main.382e002d.js"
+  }
+]);
